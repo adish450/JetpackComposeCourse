c07bd21934c28d063e69d65dcf7007de4ba58a27
adish17 <adishirfan17@gmail.com>
2025-11-11 03:32:41 +0530

    styling TextView

--- SimpleTextComposeActivity/app/src/main/java/com/androidstudioprojects/simpletextcomposeactivity/MainActivity.kt
+++ SimpleTextComposeActivity/app/src/main/java/com/androidstudioprojects/simpletextcomposeactivity/MainActivity.kt
@@ -13,12 +13,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.androidstudioprojects.simpletextcomposeactivity.ui.theme.SimpleTextComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -57,7 +62,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             .clickable {}
             .padding(horizontal = 10.dp, vertical = 10.dp)
             .width(200.dp)
-            .height(240.dp)
+            .height(240.dp),
+        style = /*TextStyle(color = Color.Blue,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp)*/
+            MaterialTheme.typography.headlineMedium,
+        fontWeight = FontWeight.Bold,
+        color = Color.Red
     )
 }
 
