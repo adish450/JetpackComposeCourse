c551a7d815916f9e551f03dc55258886c53f111b
adish17 <adishirfan17@gmail.com>
2025-11-11 02:04:23 +0530

    Button composable

--- SimpleTextComposeActivity/app/src/main/java/com/androidstudioprojects/simpletextcomposeactivity/MainActivity.kt
+++ SimpleTextComposeActivity/app/src/main/java/com/androidstudioprojects/simpletextcomposeactivity/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.androidstudioprojects.simpletextcomposeactivity.ui.theme.SimpleTextComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleTextComposeActivityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GreetingButton("Android")
                 }
             }
         }
@@ -38,10 +39,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun GreetingButton(name: String) {
+    Button({ /*TODO*/ }){
+        Greeting(name)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() { // tells the compiler that this is a preview function used to simulate as it would be in the main activity
     SimpleTextComposeActivityTheme { // make sure the code is same as the main activity so that it can be previewed exactly the same as the main activity
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingButtonPreview() { // tells the compiler that this is a preview function used to simulate as it would be in the main activity
+    SimpleTextComposeActivityTheme { // make sure the code is same as the main activity so that it can be previewed exactly the same as the main activity
+        GreetingButton("Android")
     }
 }
