dbe9e267c4599e2ae05b7004df03c1973d43c33d
adish17 <adishirfan17@gmail.com>
2025-11-11 17:33:43 +0530

    adding state list

--- DynamicContentExample/app/src/main/java/com/androidstudioprojects/dynamiccontentexample/MainActivity.kt
+++ DynamicContentExample/app/src/main/java/com/androidstudioprojects/dynamiccontentexample/MainActivity.kt
@@ -12,13 +12,14 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.androidstudioprojects.dynamiccontentexample.ui.theme.DynamicContentExampleTheme
 
-val namesList = mutableListOf<String>("Adish", "Alfi", "Hamza")
+val namesList = mutableStateListOf("Adish", "Alfi", "Hamza")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
