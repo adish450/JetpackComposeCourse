9c0091179442e4815fb9fd7600df3781f3b3e56b
adish17 <adishirfan17@gmail.com>
2025-11-11 17:27:04 +0530

    dynamimc content example

---
+++ DynamicContentExample/app/src/main/java/com/androidstudioprojects/dynamiccontentexample/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.androidstudioprojects.dynamiccontentexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.androidstudioprojects.dynamiccontentexample.ui.theme.DynamicContentExampleTheme
+
+val namesList = mutableListOf<String>("Adish", "Alfi", "Hamza")
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DynamicContentExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GreetingList(namesList)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingList(names : List<String>) {
+    Column(modifier = Modifier
+        .padding(vertical = 4.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        for (name in names) {
+            Greeting(name)
+        }
+        Button({namesList.add("Daddy")}) {
+            Text(text = "Add new name")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DynamicContentExampleTheme {
+        GreetingList(namesList)
+    }
+}
